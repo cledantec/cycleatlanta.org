2cf7050e1ed9311fc248bd7e1ae6ad351caa1b94
Christopher Le Dantec <ledantec@gatech.edu>
2013-01-30 09:52:11 -0500

    Cleaned up extranious logging.

--- include/TripFactory.php
+++ include/TripFactory.php
@@ -119,7 +119,8 @@ class TripFactory
 
 		if ( $db->query( $query ) ) 
 		{
-			Util::log( __METHOD__ . "() updated trip {$id}: {$query}" );
+			//Util::log( __METHOD__ . "() updated trip {$id}: {$query}" );
+			Util::log( __METHOD__ . "() updated trip {$id}" );
 			return self::getTrip( $id );
 		}
 		else
